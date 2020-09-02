f9fc484e7892f8ba342769b7d9ee0a00326ea28c
Sergey Shaporov <sergei.shaporov@gmail.com>
2020-09-02 23:03:10 +0300

    #08 Callback for Star Component

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,32 +1,33 @@
-import React, {useState} from "react";
+import React, {Dispatch, SetStateAction, useState} from "react";
 
 export function UncontrolledRating() {
     let [value, setValue] = useState(0);
 
-    const addStarCallback = () => {
-        alert(1);
-    }
-
     return (
             <div>
-                <Star addStarCallback={addStarCallback} selected={value > 0}/><button onClick={() => setValue(1)}>1</button>
-                <Star addStarCallback={addStarCallback} selected={value > 1}/><button onClick={() => setValue(2)}>2</button>
-                <Star addStarCallback={addStarCallback} selected={value > 2}/><button onClick={() => setValue(3)}>3</button>
-                <Star addStarCallback={addStarCallback} selected={value > 3}/><button onClick={() => setValue(4)}>4</button>
-                <Star addStarCallback={addStarCallback} selected={value > 4}/><button onClick={() => setValue(5)}>5</button>
+                <Star selected={value > 0} setValue={setValue} value={1} />
+                <Star selected={value > 1} setValue={setValue} value={2} />
+                <Star selected={value > 2} setValue={setValue} value={3} />
+                <Star selected={value > 3} setValue={setValue} value={4} />
+                <Star selected={value > 4} setValue={setValue} value={5} />
             </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
-    addStarCallback: () => void
+    value: 1 | 2 | 3 | 4 | 5
+    setValue: (value: 1 | 2 | 3 | 4 | 5 ) => void
 }
 function Star(props: StarPropsType) {
 
 
 
-    return (
-        props.selected === true ? <span onClick={props.addStarCallback}><b>star</b></span> : <span onClick={ props.addStarCallback}>star</span>
-    )
+    // return (
+    //     props.selected ?
+    //         <span onClick={props.addStarCallback}><b>star</b></span> :
+    //         <span onClick={ props.addStarCallback}>star</span>
+    // )
+
+    return <span onClick={ () => props.setValue(props.value) }>{ props.selected ? <b>star </b> : "star " }</span>
 }
