988d58023dccfcf7ed2280d38595c2f4f1db9189
Sergey Shaporov <sergei.shaporov@gmail.com>
2020-09-10 16:59:31 +0300

    #11 defaultValue

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,30 +1,29 @@
 import React, {Dispatch, SetStateAction, useState} from "react";
-import {RatingValueType} from "../Rating/Rating";
 
-
-type RatingPropsType = {
-    defaultValue?: RatingValueType
-    onChange: (value: RatingValueType) => void
-}
-export function UncontrolledRating(props: RatingPropsType) {
-    let [value, setValue] = useState<RatingValueType>(props.defaultValue ? props.defaultValue : 0);
+export function UncontrolledRatingVersion2() {
+    let [value, setValue] = useState(0);
 
     return (
-            <div>
-                <Star selected={value > 0} setValue={() => {setValue(1); props.onChange(1) }} value={1} />
-                <Star selected={value > 1} setValue={() => {setValue(2); props.onChange(2) }} value={2} />
-                <Star selected={value > 2} setValue={() => {setValue(3); props.onChange(3) }} value={3} />
-                <Star selected={value > 3} setValue={() => {setValue(4); props.onChange(4) }} value={4} />
-                <Star selected={value > 4} setValue={() => {setValue(5); props.onChange(5) }} value={5} />
-            </div>
+        <div>
+            <Star selected={value > 0} setValue={() => { setValue(1) }} />
+            <Star selected={value > 1} setValue={() => { setValue(2) }} />
+            <Star selected={value > 2} setValue={() => { setValue(3) }} />
+            <Star selected={value > 3} setValue={() => { setValue(4) }} />
+            <Star selected={value > 4} setValue={() => { setValue(5) }} />
+        </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
-    value: 1 | 2 | 3 | 4 | 5
-    setValue: (value: 1 | 2 | 3 | 4 | 5 ) => void
+    setValue: () => void
 }
+
 function Star(props: StarPropsType) {
-    return <span onClick={ () => props.setValue(props.value) }>{ props.selected ? <b>star </b> : "star " }</span>
+
+    return (
+        <span onClick={() => props.setValue()}>
+            {props.selected ? <b>star </b> : "star "}
+        </span>
+    )
 }
